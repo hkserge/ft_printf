91b275318f1af2af2b4cd5d007c0b87278635c1f
Kokou HELEGBE <khelegbe@e1r6p4.42.fr>
2021-02-11 16:13:55 +0100

    OK

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 13:27:26 by khelegbe          #+#    #+#             */
-/*   Updated: 2021/02/11 15:58:16 by khelegbe         ###   ########.fr       */
+/*   Updated: 2021/02/11 16:08:56 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	main()
 	int	myprf = 0;
 	int	prf = 0;
 
-	myprf = ft_printf("_%10.4d %d %d_\n", -23, 42, 87);
-	prf = printf("_%10.4d %d %d_\n", -23, 42, 87);
+	myprf = ft_printf("_%05d_\n", 5);
+	prf = printf("_%05d_\n", 5);
 
 	printf("Myprf = %d\n", myprf);
 	printf("prf = %d\n", prf);
