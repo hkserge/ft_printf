bb3bd92830c9612ebf793fe84a2da4a78b39a17e
hkserge <kserge.helegbe@gmail.com>
2021-03-12 09:27:54 +0100

    OK

--- srcs/ft_convert_hex.c
+++ srcs/ft_convert_hex.c
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 03:03:56 by khelegbe          #+#    #+#             */
-/*   Updated: 2021/03/09 12:55:12 by khelegbe         ###   ########.fr       */
+/*   Updated: 2021/03/12 09:24:49 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 char	*ft_convert_hex(long int var, char *base_to)
 {
 	char	*temp;
+	char	*out;
 
 	temp = ft_itoa(var);
+	out = ft_convert_base(temp, "0123456789", base_to);
 	free(temp);
-	return (ft_convert_base(temp, "0123456789", base_to));
+	return (out);
 }
